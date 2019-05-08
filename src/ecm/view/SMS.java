4e703be15baf28ce0513d2a72ee41466e74721e8
Kei <kei.wang@yahoo.com.au>
2019-05-08 22:29:23 +0800

    Still beginning

--- src/ecm/view/SMS.java
+++ src/ecm/view/SMS.java
@@ -1,10 +1,14 @@
+package ecm.view;
+
 public class SMS
 {
     public SMS()
     {
     }
 
-    /**Sends an SMS to a given phonenumber.*/
+    /**Sends an SMS to a given phonenumber
+     * @param mobileNumber.
+     * @param message*/
     public void sendSMS(long mobileNumber,String message)
     {
     }
